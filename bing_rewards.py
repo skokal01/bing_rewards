5abee6725381e287386b786365a9e9a73b2311e2
Santhosh Kokala <skokal01@gmail.com>
2015-05-15 15:46:48 -0700

    Update bing_rewards.py
    Updated bing_rewards.py with the following functionality
    
    1. Mutlitple user support
    2. Fetch hackernews stories
    3. Search hackernews stories.

--- bing_rewards.py
+++ bing_rewards.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-
+import unittest
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,198 +7,93 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+import requests
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-class BingTest(unittest.TestCase):
-    def setUp(self):
-        binary = FirefoxBinary("/Applications/Firefox.app/Contents/MacOS/firefox")
-        self.driver = webdriver.Firefox(firefox_binary=binary)
 
-        #self.driver = webdriver.Firefox()
-        #/Applications/Firefox.app/Contents/MacOS/firefox
-        self.driver.implicitly_wait(30)
-        #import pdb; pdb.set_trace()
+class FixturesTest(unittest.TestCase):
+
+   def setUp(self):
+        self.driver = None
         self.base_url = "http://www.bing.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
-    def test_bing(self):
+        self.story_titles = []
+        self.max_users = 3
+        self.max_searches = 36
+
+   def fetch_top_stories(self):
+        requests.packages.urllib3.disable_warnings()
+        hckr_news_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+        hckr_stry_url = "https://hacker-news.firebaseio.com/v0/item/%s.json"
+
+        # Get base hacker news stories
+        r = requests.get(hckr_news_url, verify=False)
+        stories = r.json()
+
+        # Get each story title
+        index = 0
+        for story in stories:
+           r = requests.get(hckr_stry_url % (story), verify=False)
+           self.story_titles.append(r.json()['title'])
+           index = index + 1
+           print index, r.json()['title']
+           if index == self.max_users * self.max_searches:
+              break
+
+   def login_bing(self, user_name, password):
         driver = self.driver
         driver.get(self.base_url + "/")
+
+        # Login
         driver.find_element_by_id("id_s").click()
         driver.find_element_by_css_selector("span.id_name").click()
         driver.find_element_by_id("i0116").clear()
-        driver.find_element_by_id("i0116").send_keys("<bing rewards username>")
+        driver.find_element_by_id("i0116").send_keys(user_name)
         driver.find_element_by_id("i0118").clear()
-        driver.find_element_by_id("i0118").send_keys("<bing rewards password>!")
+        driver.find_element_by_id("i0118").send_keys(password)
         driver.find_element_by_id("idChkBx_PWD_KMSI0Pwd").click()
         driver.find_element_by_id("idSIButton9").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123456")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234567")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345678")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123456789")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234567890")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345678901")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123456789012")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234567890123")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345678901234")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123456789012345")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234567890123456")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345678901234567")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123456789012345678")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234567890123456789")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345678901234567890")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123456789012345678901")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234567890123456789012")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345678901234567890123")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("1234567890123456789012345678901234")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("12345678901234567890123456789012345")
-        driver.find_element_by_id("sb_form_go").click()
-        driver.find_element_by_id("sb_form_q").click()
-        driver.find_element_by_id("sb_form_q").clear()
-        driver.find_element_by_id("sb_form_q").send_keys("123456789012345678901234567890123456")
-        driver.find_element_by_id("sb_form_go").click()
+
+   def logout_bing(self):
+        # Logout
+        driver = self.driver
         driver.find_element_by_id("id_l").click()
         driver.find_element_by_css_selector("span.id_name").click()
         driver.find_element_by_id("id_l").click()
-        driver.find_element_by_css_selector("span.id_name").click()
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
+        driver.find_element_by_css_selector("span.id_name").click()              
+
+   def query_bing(self):
+        driver = self.driver
+        for x in range(0, self.max_searches):
+           queryStr = self.story_titles.pop()
+           driver.find_element_by_id("sb_form_q").clear()
+           driver.find_element_by_id("sb_form_q").send_keys(queryStr)
+           driver.find_element_by_id("sb_form_go").click()
+           driver.find_element_by_id("sb_form_q").click()
+                   
+   def get_rewards(self, user_name, password):
+        self.login_bing(user_name, password)
+        self.query_bing()
+        time.sleep(3)
+        self.logout_bing()
+
+   def test_bing(self):
+        # get top story titles from hackernews
+        self.fetch_top_stories()
+
+        # Launch firefox web-browser
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+                
+        self.get_rewards("<USER1>", "<USER1 PASSWORD>")
+        self.get_rewards("<USER2>", "<USER2 PASSWORD>")
+        self.get_rewards("<USER3>", "<USER3 PASSWORD>")
+
+   def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
